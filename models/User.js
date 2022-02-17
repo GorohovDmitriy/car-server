fec6d785ac1ccfd96d109a8a791bb33e26e78814
Gorohov <d.gorohov@itransition.com>
2022-02-17 14:14:31 +0300

    corrected gitignore

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require("mongoose");
 
-const schema = new Schema({
+const User = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: true },
   name: { type: String },
-  role: { type: String, default: "reader" },
+  roles: [{ type: String, ref: "Role" }],
 });
 
-module.exports = model("User", schema);
+module.exports = model("User", User);
