c7c0f44d497ae16977ce3611859cf8444b8eb9ac
Gorohov <d.gorohov@itransition.com>
2022-03-01 16:49:43 +0300

    added editCar in controller

--- routes/Cars.js
+++ routes/Cars.js
@@ -5,5 +5,6 @@ const controller = require("../controllers/carsControllers");
 router.post("/create", controller.addCar);
 router.get("/get", controller.getCar);
 router.get("/details/:id", controller.getCarId);
+router.put("/edit/:id", controller.editCar);
 
 module.exports = router;
