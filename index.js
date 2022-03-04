31f12bb7fe82af19bf120d477c15078a44c6968e
Gorohov <d.gorohov@itransition.com>
2022-03-04 10:39:31 +0300

    remove origin

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const carsRoute = require("./routes/Cars");
 const cors = require("cors");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://store-tesla-front.herokuapp.com/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/cars", carsRoute);
