1418d6b8ea4a63f854fcd7afe23812f3a8c27a47
Gorohov <d.gorohov@itransition.com>
2022-03-04 10:44:39 +0300

    add procfile

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/cars", carsRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
     await mongoose.connect(
